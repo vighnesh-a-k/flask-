91f748b4d31b1ae96e2b6e22598ec40284382a75
vighnesh <19br13016@rit.ac.in>
2022-06-17 21:43:57 +0530

    commit2

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ db=SQLAlchemy(app)
 
 class todo(db.Model):
     id=db.Column(db.Integer, primary_key=True)
-    content=db.column(db.String(200))
+    content=db.Column(db.String(200))
     completed=db.Column(db.Integer,default=0)
+   
     def __init__(self, c="gava"):
         self.content=c
 
@@ -23,20 +24,34 @@ db.create_all()
 @app.route("/",methods=["POST","GET"])
 def index():
     if request.method=="POST":
-        #task_content=request.form["content"]
-        newtask=todo()
+        task_content=request.form["content"]
+        newtask=todo(task_content)
+        
+        print(task_content)
         try:
             db.session.add(newtask)
             db.session.commit()
             return redirect("/")
         except:
-            return("blah blah blah")
+            return "error"
          
 
     else:
         task=todo.query.order_by(todo.id).all()
 
         return render_template('index.html',tasks=task)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_del=todo.query.get(id)
+    
+    try:
+        db.session.delete(task_del)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return("oops")
+
 
 
 
